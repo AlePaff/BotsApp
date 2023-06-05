17079a7c5d9d8941a4135e0ef49e88a0f7d58380
AlePaff <alejandropaff@gmail.com>
2023-06-04 22:42:34 -0300

    smeta module bug fix length undefined or type error message

--- modules/smeta.ts
+++ modules/smeta.ts
@@ -73,7 +73,6 @@ export = {
                 }
                 return true;
             }
-
             //if all arguments are null returns error
             if (validateArguments(args)) {
                 await client.sendMessage(
@@ -87,33 +86,38 @@ export = {
             //it allows more than 1 emoji but its a secret for now (cuz its too picky and buggy)
             //convert from string separate by commas to Categories
             const arg_emojis: Categories[] = [];
-            for (let i = 0; i < args[0].split(",").length; i++) {
-                arg_emojis.push(args[0].split(",")[i] as Categories);
-            }
+            // if (args.length > 0) {
+                for (let i = 0; i < args[0].split(",").length; i++) {
+                    arg_emojis.push(args[0].split(",")[i] as Categories);
+                }
+            // }
 
             //cast string emojis: [ '💩', '🐶' ] to type Categories['💩', '🐶']
             const cast_to_categories = (arr: string[]): Categories[] => {
+                if (arr === undefined || arr === null) { return [] }
                 return arr.map(item => item as Categories);
             }
 
             let metadata = {
-                args_length: args.length,
+                args_length: args?.length,
                 categories: args[0] != "null" ? arg_emojis : cast_to_categories(metadata_original["emojis"]),
                 type: args[1] != "null" ? getStickerType(args[1]) : StickerTypes.FULL,
                 // pack_name: args[2] ?? metadata_original['sticker-pack-name'],
-                pack_name: (args[2] == "null" || args[2].length == 0) ? metadata_original['sticker-pack-name'] : args[2],
+                pack_name: (args[2] == "null" || args[2]?.length == 0) ? metadata_original['sticker-pack-name'] : args[2],
                 // author_name: args[3] ?? metadata_original['sticker-pack-publisher'],
-                author_name: (args[3] == "null" || args[3].length == 0) ? metadata_original['sticker-pack-publisher'] : args[3],
+                author_name: (args[3] == "null" || args[3]?.length == 0) ? metadata_original['sticker-pack-publisher'] : args[3],
                 id: metadata_original['sticker-id'],
                 // quality: 100, // The quality of the output file
                 // background: '#000000' // The sticker background color (only for full stickers)
+                // android_app_store_link: metadata_original['android-app-store-link'],
             }
-
+            
             const sticker = new Sticker(filePath, {
                 categories: metadata.categories,
                 type: metadata.type,
                 pack: metadata.pack_name,
                 author: metadata.author_name,
+                id: metadata.id,
             })
 
             await sticker.toFile(fileWebp)
